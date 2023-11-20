a570b78ff64b31370b513435097160be80c222b1
micbmoor <micbmoor@tesla.cs.iupui.edu>
2023-11-20 14:53:08 -0500

    Finished OPTAB, generate intermediate form of the program

--- FileOutput.java
+++ FileOutput.java
@@ -1,6 +1,8 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * FileOutput
@@ -15,7 +17,23 @@ public class FileOutput {
         this.fileName = fileName;
     }
 
-    void writeFile(ObjectProgram output) {
+    void writeIntermediateFile(List<Instruction> instructionList) {
+	try(BufferedWriter writer = new BufferedWriter(new FileWriter("Intr" + this.fileName))) {
+		for (int i = 0; i < instructionList.size(); i++) {
+			Instruction instruction = instructionList.get(i);
+			writer.write(instruction.getLoc() + " ");
+			writer.write(instruction.getMnemonic() + " ");
+			writer.write(instruction.getOperands() + " ");
+			//writer.write(instruction.getObjCode());
+			writer.newLine();
+		}
+	} catch (IOException e) {
+		System.out.println("could not write intermediate file");
+	}
+    }
+
+
+    void writeObjectFile(ObjectProgram output) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(this.fileName))) {
             writer.write(output.Head);
             writer.newLine();
--- Main.java
+++ Main.java
@@ -3,17 +3,105 @@ import java.util.List;
 public class Main {
     
     public static void main(String[] args) {
+	//pass 1
+	int LOCCTR = 0;
+	OPTABHashTable OPTAB = new OPTABHashTable();
         String fileName = "basic";
         FileInput fileParser = new FileInput(fileName+".txt");
         List<Instruction> basicInstructions = fileParser.getParsedInstructions();
-        basicInstructions.forEach((Instruction inst) -> printInstruction(inst));
+        for (int i = 0; i < basicInstructions.size(); i++) {
+		Instruction instruction = basicInstructions.get(i);
+		String opcode = OPTAB.findOperation(instruction.getMnemonic());
+		if (opcode != "") {
+			String hexLoc = convertToHex(LOCCTR, 4);
+			basicInstructions.get(i).setLoc(hexLoc);
+			LOCCTR += 3;
+		}
+		else {
+			opcode = instruction.getMnemonic();
+			if (opcode.equals("WORD")) {
+				System.out.println("word");
+			}
+			else if (opcode.equals("RESB")) {
+				System.out.println("resb");
+			}
+			else if (opcode.equals("RESW")) {
+				int operand = Integer.parseInt(instruction.getOperands());
+				String hexLoc = convertToHex(LOCCTR, 4);
+				basicInstructions.get(i).setLoc(hexLoc);
+				LOCCTR += 3 * operand;
 
-        ObjectProgram output = new ObjectProgram();
+			}
+			else if (opcode.equals("BYTE")) {
+				System.out.println("byte");
+			}
+			else {
+				System.out.println(opcode);
+				System.out.println("ERROR: invalid operation");
+				System.exit(0);
+			}
+		}
+
+	}
+
+	FileOutput writer = new FileOutput("test.txt");
+	writer.writeIntermediateFile(basicInstructions);
+
+        /*ObjectProgram output = new ObjectProgram();
         output.Head = "testHead";
         output.Text = "testText";
         output.End = "testEnd";
         FileOutput printer = new FileOutput(fileName+".o");
-        printer.writeFile(output);
+        printer.writeFile(output);*/
+    }
+
+	static String convertToHex(int convertNum, int length) {
+		int quotient = convertNum;
+	    	int remainder = convertNum;
+	    	String reverseHexNumber = "";
+		while (quotient != 0) {
+			remainder = quotient % 16;
+			quotient = quotient / 16;
+			if (remainder >= 10 && remainder <= 15){
+				switch (remainder) {
+					case 10:
+					       reverseHexNumber += "A";
+					       break;
+					case 11:
+					       reverseHexNumber += "B";
+					       break;
+					case 12:
+					       reverseHexNumber += "C";
+					       break;
+					case 13:
+					       reverseHexNumber += "D";
+					       break;
+					case 14:
+					       reverseHexNumber += "E";
+					       break;
+					case 15:
+					       reverseHexNumber += "F";
+					       break;
+				}
+			}
+			else reverseHexNumber += Integer.toString(remainder);
+		}
+		String hexNumber = "";
+		for (int i = reverseHexNumber.length() - 1; i >= 0; i--){
+			hexNumber += reverseHexNumber.charAt(i);
+		}
+		if (hexNumber.length() < length) {
+			String padding = "";
+			int i = hexNumber.length();
+			while (i < length){
+				padding += "0";
+				i++;
+			}
+			padding += hexNumber;
+			return padding;
+		}
+		else return hexNumber;
+
     }
 
     static void printInstruction(Instruction inst) {
